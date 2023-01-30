36aefdae3250c97e632b7cbe382492ea61e3896d
Choy <lll0858630@gmail.com>
2023-01-30 19:45:13 +0900

    줄높이 맞춤

--- front/src/modules/detailDescSet.js
+++ front/src/modules/detailDescSet.js
@@ -1,6 +1,6 @@
 const detailDescSet = (title, content) => {
   return `  
-      <div class="descs" style="color: #d9d9d9;font-size: 14pt;">
+      <div class="descs" style="color: #d9d9d9;font-size: 14pt; line-height: 130%;">
         <span id="imgPlace"></span>
         <span style="font-weight: bold;">${title}</span>
         <span style="border-right: 2px solid #d9d9d9;margin: 0 10px 0 6px;"></span>

b1725d4e5a42e9c2919680ab9c291837cd5b1e4c
Choy <lll0858630@gmail.com>
2023-02-01 21:43:10 +0900

    수정

--- front/src/routers/Router.js
+++ front/src/routers/Router.js
@@ -5,7 +5,6 @@ import CONTACT from "../page/Contact.js";
 import ETC from "../page/Etc.js";
 import NotFound from "../page/NotFound.js";
 import SUBMIT from "../page/submit.js";
-import DETAILS from "../page/DETAILS.js";
 
 const router = async () => {
   const routes = [
@@ -15,7 +14,6 @@ const router = async () => {
     { path: '/CONTACT', view: CONTACT },
     { path: '/ETC', view: ETC },
     { path: '/MAIN', view: MAIN },
-    { path: '/DETAILS', view: DETAILS },
   ];
   const pageMatches = routes.map((route) => {
     return {

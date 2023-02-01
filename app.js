64b08348db8379b9d1e922209e352e14ae56300d
Choy <lll0858630@gmail.com>
2023-02-01 20:35:45 +0900

    약간 수정

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3020; //포트 지정
  * *정적 파일 루트 지정
  * path.resolve로 인자로 받은 값들을 하나의 문자열로 만든 뒤 정적 디렉토리에 대한 마운트 경로 지정
  * app.js 실행경로 + "/path"를 localhost:port/path로 마운트
+ * app.use('{설정하고자 하는 web상의 경로}', express.static('{설정하고자 하는 실제 static 경로}'));
  */
 app.use('/front', express.static(path.resolve(__dirname, 'front')));
 app.use('/src', express.static(path.resolve(__dirname, 'front', 'src')));
